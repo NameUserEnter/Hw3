dbab12b340aac001b05dbef5526ff37d06b533eb
NameUserEnter <92257494+NameUserEnter@users.noreply.github.com>
2021-10-11 19:11:01 +0300

    Hw
    hw

---
+++ 3)/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Co
+{
+    class Program
+    {
+        public abstract class Student
+        {
+            
+            public Student(string name)
+            {
+                this.name = name;
+                state = "";
+            }
+            public void Read()
+            {
+                state += "Read ";
+            }
+            public void Write()
+            {
+                state += "Write";
+            }
+            public void Relax()
+            {
+                state += "Relax";
+            }
+            public abstract void Study();
+            public string Name
+            {
+                get => name;
+                set => name = value;
+            }
+            public string State
+            {
+                get => state;
+                set => state = value;
+            }
+            protected string name;
+            protected string state;
+
+        }
+
+        public class GoodStudent:Student
+        {
+            public GoodStudent(string name) : base(name)
+            {
+                state += "good";
+            }
+            public override void Study()
+            {
+                Read(); Write(); Read(); Write(); Relax();
+            }
+        }
+
+        public class BadStudent : Student
+        {
+            public BadStudent(string name) : base(name)
+            {
+                state += "bad";
+            }
+            public override void Study()
+            {
+                Relax(); Relax(); Relax(); Relax(); Relax();
+            }
+        }
+
+        public class Group
+        {
+            public Group(string nameGroup) 
+            {
+                this.nameGroup = nameGroup;
+                listStud = new List<Student>();
+            }
+            public void AddStudent(Student st)
+            {
+                listStud.Add(st);
+            }
+            public string GetInfo()
+            {
+                string line = "";
+                foreach (Student x in listStud)
+                {
+                    line += x.Name +  " ";
+                }
+                return nameGroup + " " + line;
+            }
+            public string GetFullInfo()
+            {
+                string line = "";
+                foreach (Student x in listStud)
+                {
+                    line += x.Name + " - " + x.State + " ";
+                }
+                return nameGroup + " " + line;
+            }
+            private string nameGroup;
+            private List<Student> listStud;
+
+        }
+
+        static void Main(string[] args)
+        {
+
+            BadStudent st1 = new BadStudent("Oleksii");
+            BadStudent st2 = new BadStudent("Dima");
+            GoodStudent st3 = new GoodStudent("Oleksii");
+            List<Student> stt = new List<Student> { st1, st2, st3 };
+            Group first = new Group("K-25");
+            foreach (Student s in stt)
+            {
+                first.AddStudent(s);
+            }
+            Console.WriteLine(first.GetInfo());
+            Console.WriteLine(first.GetFullInfo());
+
+        }
+    }
+}
---
+++ 4)/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Consol
+{
+    class Converter
+    {
+        public double usd;
+        public double eur;
+
+        public Converter(double usd, double eur)
+        {
+            this.usd = usd;
+            this.eur = eur;
+        }
+
+        public double FromHrToEur(double hr)
+        {
+            return hr / eur;
+        }
+
+        public double FromHrToUsd(double hr)
+        {
+            return hr / usd;
+        }
+
+        public double FromEurToHr(double hr)
+        {
+            return eur * hr;
+        }
+
+        public double FromUsdToHr(double hr)
+        {
+            return usd * hr;
+        }
+        public double FromUsdToEur(double hr)
+        {
+            return (hr * usd) / eur;
+        }
+        public double FromEurToUsd(double hr)
+        {
+            return (hr * eur) / usd;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Converter first = new Converter(26.37, 30.52);
+            Console.WriteLine("Перевести з: hrn, eur, usd.");
+            string a = Console.ReadLine();
+            Console.WriteLine("Конвертувати в: hrn, eur, usd.");
+            string b = Console.ReadLine();
+            Console.WriteLine("Сума для переводу");
+            double c;
+            c = double.Parse(Console.ReadLine());
+            if (a == b)
+            {
+                Console.WriteLine(c + " " + a + " = " + c + " " + a);
+            }
+            if (a == "eur")
+            {
+                if (b == "hrn") Console.WriteLine(c + " " + a + " = " + first.FromEurToHr(c) + " " + b);
+                if (b == "usd") Console.WriteLine(c + " " + a + " = " + first.FromEurToUsd(c) + " " + b);
+            }
+
+            if (a == "hrn")
+            {
+                if (b == "usd") Console.WriteLine(c + " " + a + " = " + first.FromHrToUsd(c) + " " + b);
+                if (b == "eur") Console.WriteLine(c + " " + a + " = " + first.FromHrToEur(c) + " " + b);
+            }
+
+            if (a == "usd")
+            {
+                if (b == "hrn") Console.WriteLine(c + " " + a + " = " + first.FromUsdToHr(c) + " " + b);
+                if (b == "eur") Console.WriteLine(c + " " + a + " = " + first.FromUsdToEur(c) + " " + b);
+            }
+        }
+    }
+}
